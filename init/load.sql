d277cce435c6865425320ab6a09c7d57cb11ee87
Joon Young Lee <joonyoung.lee@duke.edu>
2021-10-08 21:12:07 -0400

    added more to db structure

--- init/load.sql
+++ init/load.sql
@@ -1,2 +1,4 @@
 \COPY Users FROM './init/data/Users.csv' WITH DELIMITER ',' NULL '' CSV;
-\COPY Quotes FROM './init/data/Quotes.csv' WITH DELIMITER ',' NULL '' CSV;
+\COPY Symbol FROM './init/data/Symbols.csv' WITH DELIMITER ',' NULL '' CSV; 
+\COPY Quote FROM './init/data/Quotes.csv' WITH DELIMITER ',' NULL '' CSV;
+\COPY Profile FROM './init/data/Profiles.csv' WITH DELIMITER ',' NULL '' CSV;
