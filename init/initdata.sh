4eb929d4addb31ff9a43d2f60414d83ba8c64057
Joon Young Lee <joonlee7890@gmail.com>
2021-10-11 16:30:57 -0400

    db changes needed for auth. to work

--- init/initdata.sh
+++ init/initdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 psql -af ./init/create.sql quantx 
-psql -af ./init/load.sql quantx 
+# psql -af ./init/load.sql quantx 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash 
 
-docker build -t timedb .
-docker run -d --name timedb -p 5432:5432 timedb
+docker build -t timedb . && docker run -d --name timedb -p 5432:5432 timedb
