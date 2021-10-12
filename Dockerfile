aea3da00eab75958bb5b9514e31b9214da62eda9
Joon Young Lee <joonlee7890@gmail.com>
2021-10-11 22:10:04 -0400

    added data, but MARKING OUT load.sql b/c it's a bit buggy

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ ENV POSTGRES_USER root
 ENV POSTGRES_PASSWORD password 
 ENV POSTGRES_DB quantx
 COPY init/create.sql /docker-entrypoint-initdb.d/
-# COPY init/load.sql /docker-entrypoint-initdb.d/
+COPY init/load.sql /docker-entrypoint-initdb.d/
 COPY . .
--- init/load.sql
+++ init/load.sql
@@ -1,4 +1,3 @@
-\COPY Users FROM './init/data/Users.csv' WITH DELIMITER ',' NULL '' CSV;
-\COPY Symbol FROM './init/data/Symbols.csv' WITH DELIMITER ',' NULL '' CSV; 
-\COPY Quote FROM './init/data/Quotes.csv' WITH DELIMITER ',' NULL '' CSV;
-\COPY Profile FROM './init/data/Profiles.csv' WITH DELIMITER ',' NULL '' CSV;
+-- \COPY Users FROM './init/data/Users.csv' WITH DELIMITER ',' NULL '' CSV;
+-- \COPY Symbol FROM './init/data/Symbols.csv' WITH DELIMITER ',' NULL '' CSV; 
+-- \COPY Quote FROM './init/data/Quotes.csv' WITH DELIMITER ',' NULL '' CSV;
