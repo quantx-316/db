c93ca84e5e533d485f79fddfc83c1527d9885f63
James Shen <jieran.shen@duke.edu>
2021-10-28 01:30:53 -0400

    Implemented fast-forwarding

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -5,6 +5,7 @@ import ast
 import psycopg2
 from pgcopy import CopyManager
 from datetime import datetime, timedelta
+from time import sleep
 CONNECTION = "postgres://root:password@localhost:5432/quantx"
 
 # this probably needs to be changed
@@ -13,33 +14,45 @@ symbols = ['AAPL']
 def main():
     datetimes = calculate_increments()
     responses = api_call(datetimes)
-    write_db(datetimes, responses)
+    write_db(responses)
     
 
 
 def calculate_increments():
-    '''Return two list of (start, end) for all 1-min increments in the past hour, both as datetime objects'''
+    '''Return list of (start, end) for all 1-min increments in the past hour as datetime objects'''
     present = datetime.now()
     hour_ago = present - timedelta(hours = 1)
-    current_hour = hour_ago.isoformat(timespec='hours')
-    datetimes = []
-    for t in range(60):
-        current_minute = datetime.fromisoformat(current_hour) + timedelta(minutes = t)
-        datetimes.append(current_minute)
-    #print(datetimes)
-    return datetimes
+    start_hour = hour_ago.isoformat(timespec='hours')
+    start_hour = datetime.fromisoformat(start_hour)
+    print(start_hour)
+    end_hour = start_hour + timedelta(minutes=59)
+    print(end_hour)
+    return [start_hour, end_hour]
 
 
 def api_call(datetimes):
 
     responses = []
+    assert len(datetimes) == 2, "More tha two datetimes are provided."
     start_time = int(datetimes[0].timestamp())
-    end_time = int((datetimes[-1]+timedelta(minutes=1)).timestamp()) # adding one extra minute to go to the end
-    
-    # make the call
+    end_time = int(datetimes[1].timestamp())
     finnhub_client = finnhub.Client(api_key=finn_hub_key)
+
+    # make the call
+
     for symbol in symbols:
-        response = finnhub_client.stock_candles('AAPL', '1', start_time, end_time)
+        for _ in range(10):
+            try:
+                response = finnhub_client.stock_candles(symbol, '1', start_time, end_time)
+            except finnhub.FinnhubAPIException as e:
+                if  e.status_code == 429:
+                    sleep(60)
+                    continue
+                else:
+                    raise e
+            break
+        else:
+            raise RuntimeWarning(f"API Call Failed for symbol {symbol}, {start_time = }, {end_time = }.")
         #print(response)
         responses.append(response)
 
@@ -54,7 +67,7 @@ def api_call(datetimes):
     # response_text = lines[0].strip()
     return responses
 
-def write_db(datetimes, responses):
+def write_db(responses):
     conn = psycopg2.connect(CONNECTION)
     cursor  = conn.cursor
     cols=['time', 'symbol', 'price_open', 'price_high', 'price_low', 'price_close']
@@ -63,6 +76,7 @@ def write_db(datetimes, responses):
         response = responses[i]
         if response['s'] != 'ok': # out of market time, etc.
             continue
+        datetimes = [datetime.fromtimestamp(x) for x in response['t']]
         data = [list(a) for a in zip(
             datetimes, 
             [symbol]*len(datetimes), 
